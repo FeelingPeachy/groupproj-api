ef663c29899afaa05c641b157aa57fbff636edde
Travk33 <travis.kerali@yahoo.co.uk>
2025-08-20 14:30:01 +0100

    created routes blueprint

---
+++ routes/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/pokemonController.controller');
+
+// this routes folder is supposed to be for pokemons
+
+// -------------------------TODO: Implement Pokemon routes----------------------------
+
+// get /pokemon/:name
+
+router.get('/pokemon/:name', PokemonController.getPokemonByName);
+
+// POST (ADD) to pokedeck
+
+router.post('/pokedeck', PokemonController.addPokemonToDeck);
+
+// get /active-deck
+router.get('/active-deck', PokemonController.getActiveDeck);
+
+// delete-from-deck/:id
+router.delete('/delete-from-deck/:id', PokemonController.removeFromDeck);
+
+
+module.exports = router;
