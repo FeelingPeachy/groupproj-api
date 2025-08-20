9b3a211edbc4898ac4b5b697fbf37ea6b0870216
zxch91 <lolget360ed@gmail.com>
2025-08-20 14:36:08 +0100

    original model for pokemon

---
+++ models/pokemon.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const Pokemon = sequelize.define("Pokemon", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        active: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: 
+        }
+    });
+
+    return Pokemon;
+}
