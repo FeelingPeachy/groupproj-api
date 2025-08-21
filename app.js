2ff38e7d05b80490bb0175ccf35db09bd94177ee
Travk33 <travis.kerali@yahoo.co.uk>
2025-08-21 09:22:23 +0100

    configuring connecting to database and having a utils folder

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+const sequelize = require('./utils/connectToDB');
+
+
 const express = require('express');
 const app = express();
 
 require('dotenv').config();
 const port = 3000;
 
+sequelize.sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 //middlewares
 app.use(express.json());
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
--- models/pokemon.js
+++ models/pokemon.js
@@ -1,35 +1,36 @@
-export default (sequelize, DataTypes) => {
-    const Pokemon = sequelize.define("Pokemon", {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        type: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        height: {
-            type: DataTypes.FLOAT,
-            allowNull: false
-        },
-        weight: {
-            type: DataTypes.FLOAT,
-            allowNull: false
-        },
-        base_experience: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        active: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        }
-    });
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connectToDB');
 
-    return Pokemon;
-}
+const Pokemon = sequelize.define("Pokemon", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    height: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    weight: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    base_experience: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+});
+
+module.exports = Pokemon;
